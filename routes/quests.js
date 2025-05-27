aea8060aa0148cd05eb2011bf5e863734b402a73
Fryndon <153004335+Fryndon@users.noreply.github.com>
2025-05-27 21:21:26 +0300

    Delete routes directory

--- routes/quests.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const questController = require("../controllers/quests");
-
-router.get("/", questController.getAllQuests);
-router.post("/", questController.createQuest);
-
-module.exports = router;
